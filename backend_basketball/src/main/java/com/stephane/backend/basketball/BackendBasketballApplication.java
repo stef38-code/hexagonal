1cf625bdadd2d65f19c69a6fe2349d0b41f5445e
stephane betton <stphane.betton@ag2rlamondiale.fr>
2021-07-16 09:34:07 +0200

    Initial commit

---
+++ backend_basketball/src/main/java/com/stephane/backend/basketball/BackendBasketballApplication.java
@@ -0,0 +1,13 @@
+package com.stephane.backend.basketball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBasketballApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBasketballApplication.class, args);
+    }
+
+}
