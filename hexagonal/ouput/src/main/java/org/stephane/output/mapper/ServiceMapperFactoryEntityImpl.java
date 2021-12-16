0d8d625e64887ffc4902179b307493dfa1b8c3cb
stephane betton <stphane.betton@ag2rlamondiale.fr>
2021-12-16 15:48:44 +0100

    Add TU and Builder

--- hexagonal/ouput/src/main/java/org/stephane/output/mapper/ServiceMapperFactoryEntityImpl.java
+++ hexagonal/ouput/src/main/java/org/stephane/output/mapper/ServiceMapperFactoryEntityImpl.java
@@ -2,9 +2,9 @@ package org.stephane.output.mapper;
 
 import org.mapstruct.factory.Mappers;
 
-public class ServiceMapperFactoryEntityImpl<OUT, DOM> extends ServiceMapperFactoryEntity<Object, Object> {
+public class ServiceMapperFactoryEntityImpl<O, D> extends ServiceMapperFactoryEntity<O, D> {
     @Override
-    public ServiceMapperEntity<Object, Object> create(TypeServiceMapperEntity type) {
+    public ServiceMapperEntity<O, D> create(TypeServiceMapperEntity type) {
         if (type == TypeServiceMapperEntity.PERSONNE) {
             ServiceMapperEntity mapper = Mappers.getMapper(PersonneEntityMapper.class);
             return mapper;
