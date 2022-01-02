160188de46314f7984d28476cb55bb83a469b107
b_stef <stephane.betton@gmail.com>
2022-01-02 12:09:50 +0100

    Add report

--- hexagonal/domain/src/main/java/org/stephane/domain/outils/AdresseValidation.java
+++ hexagonal/domain/src/main/java/org/stephane/domain/outils/AdresseValidation.java
@@ -3,6 +3,7 @@ package org.stephane.domain.outils;
 import java.util.Map;
 
 public class AdresseValidation extends OutilsValidation {
+
     private static final String REGEX = "\\d{5}$";
 
     public static void valideCodePostal(String champ, String codePostalValue, Map<String,String> erreurs) {
--- hexagonal/domain/src/main/java/org/stephane/domain/outils/OutilsValidation.java
+++ hexagonal/domain/src/main/java/org/stephane/domain/outils/OutilsValidation.java
@@ -4,37 +4,36 @@ import java.util.Map;
 
 public class OutilsValidation {
 
-// --Commented out by Inspection START (16/12/2021 14:06):
-//    protected OutilsValidation() {
-//    }
-// --Commented out by Inspection STOP (16/12/2021 14:06)
-    public static void notNullNotEmpty(String champ, String value, Map<String,String> erreurs) {
+    public static void notNullNotEmpty(String champ, String value, Map<String, String> erreurs) {
         if (isNull(value)) {
-            erreurs.put(champ,"Le ".concat(champ).concat(" est obligatoire !!"));
+            erreurs.put(champ, "Le ".concat(champ).concat(" est obligatoire !!"));
         } else {
             if (isEmpty(value)) {
-                erreurs.put(champ,"Le ".concat(champ).concat(" ne peut pas être vide !!"));
+                erreurs.put(champ, "Le ".concat(champ).concat(" ne peut pas être vide !!"));
             }
         }
     }
 
-    public static void notNull(String champ, Object value, Map<String,String> erreurs) {
+    public static void notNull(String champ, Object value, Map<String, String> erreurs) {
         if (isNull(value)) {
-            erreurs.put(champ,"Le ".concat(champ).concat(" est obligatoire !!"));
+            erreurs.put(champ, "Le ".concat(champ).concat(" est obligatoire !!"));
         }
     }
-    public static void notEmpty(String champ, String value, Map<String,String> erreurs) {
+
+    public static void notEmpty(String champ, String value, Map<String, String> erreurs) {
         if (isEmpty(value)) {
-            erreurs.put(champ,"Le ".concat(champ).concat(" ne peut pas être vide !!"));
+            erreurs.put(champ, "Le ".concat(champ).concat(" ne peut pas être vide !!"));
         }
     }
 
     public static boolean isEmpty(String value) {
         return value.length() == 0;
     }
+
     public static boolean isNotEmpty(String value) {
         return value.length() > 0;
     }
+
     public static boolean isNull(Object value) {
         return value == null;
     }
